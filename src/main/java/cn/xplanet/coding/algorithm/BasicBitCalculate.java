0cf044c30610ce480ff4c8ce30d2229516060e0b
peter <pjian678988@gmail.com>
2017-09-14 16:55:24 +0800

    add known

--- src/main/java/cn/xplanet/coding/algorithm/BasicBitCalculate.java
+++ src/main/java/cn/xplanet/coding/algorithm/BasicBitCalculate.java
@@ -9,7 +9,21 @@ public class BasicBitCalculate {
 		return (x & (x-1)) == 0;
 	}
 
-	/** 
+	/**
+	 * 交换两个数
+	 * @param x
+	 * @param y
+	 */
+	public static void swap(int x, int y){
+		x = x ^ y ^ (y = x);
+		System.out.println("x = " + x + "; y = " + y);
+	}
+	public static void swap2(int x, int y){
+		x ^= y; y ^= x; x ^= y;
+		System.out.println("x = " + x + "; y = " + y);
+	}
+
+	/**
 	 * 描述该方法的功能及算法流程
 	 *
 	 * @autor: Administrator  2015-3-11 下午2:29:20
@@ -18,6 +32,8 @@ public class BasicBitCalculate {
 	 */
 	public static void main(String[] args) {
 		System.out.println(isPowerOfTwo(8));
+		swap(3, 4);
+		swap2(3, 4);
 	}
 
 }
